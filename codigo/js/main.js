3d18b7368104322db3f6c1cc5afe44405178d1e8
EduardoAtene <eduardoatenesilvae@gmail.com>
2022-06-01 21:07:12 -0300

    enviar alteracoes

---
+++ codigo/js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    let logado = JSON.parse(localStorage.getItem('logado'));  
+
+    
+    $("#sairSessao").click(function(){
+        localStorage.removeItem('logado');
+        window.location.reload();
+
+    })
+
+    $("#cadastrarNoSite").click(function(){
+        window.location.href = "./index.html";
+    })
+
+    if(logado==null) {  
+        debugger
+        $("#nomeLogadoPessoa").hide();
+        $("#cadastrarNoSite").show();
+
+    }else{
+        users = JSON.parse(localStorage.getItem('usuarios'))[logado.id];
+        $("#nomeLogadoPessoa").text(users.nome);
+        $("#nomeLogadoPessoa").show();
+        $("#cadastrarNoSite").hide();
+
+
+        // if($)
+    }
+    var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+    var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)});
+
+
+})
